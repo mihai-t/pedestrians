3748ad3df075fb8aaff2a644eff62fe094e502b3
Mihai.Teletin <teletinmihai@yahoo.com>
2017-04-28 22:43:05 +0300

    test image crop and display

---
+++ src/main/java/ro/ubbcluj/cs/test/ImageReader.java
@@ -0,0 +1,63 @@
+package ro.ubbcluj.cs.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Mihai Teletin on 28-Apr-17.
+ */
+public class ImageReader {
+
+
+    static class Crop {
+        byte[] content;
+        int x, y;
+
+        public Crop(byte[] content, int x, int y) {
+            this.content = content;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    private static final Log log = LogFactory.getLog(ImageReader.class);
+
+    public static List<Crop> crop(final File image, final int offsetX, final int offsetY) throws IOException {
+
+        final BufferedImage originalImage = ImageIO.read(image);
+
+        final String fileName = image.getName().replace(".pgm", "");
+        final int width = originalImage.getWidth();
+        final int height = originalImage.getHeight();
+
+        log.debug(String.format("Original image %s loaded. Dimension: %d x %d", fileName, width, height));
+        final List<Crop> images = new ArrayList<>();
+        for (int w = offsetX; w < width; w += 48) {
+            for (int h = offsetY; h < height; h += 96) {
+                if (w + 48 > width || h + 96 > height) {
+                    continue;
+                }
+                final BufferedImage subImage = originalImage.getSubimage(w, h, 48, 96);
+
+                final ByteArrayOutputStream output = new ByteArrayOutputStream();
+                if (!ImageIO.write(subImage, "pnm", output)) {
+                    log.error("failed to crop " + fileName);
+                }
+
+                images.add(new Crop(output.toByteArray(), w, h));
+            }
+        }
+
+        return images;
+    }
+
+
+}
---
+++ src/main/java/ro/ubbcluj/cs/test/ImageTest.java
@@ -0,0 +1,55 @@
+package ro.ubbcluj.cs.test;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class ImageTest {
+    static class ImageFrame extends JFrame {
+        ImageFrame(byte[] image) throws IOException {
+            setTitle("Daimler");
+            setSize(640, 480);
+            add(new ImageComponent(image));
+        }
+
+        void showImage(final byte[] image) throws IOException {
+            add(new ImageComponent(image));
+            this.repaint();
+        }
+
+    }
+
+    private static class ImageComponent extends JComponent {
+
+        private Image image;
+
+        ImageComponent(final byte[] src) throws IOException {
+            image = ImageIO.read(new ByteArrayInputStream(src));
+        }
+
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (image == null) {
+                return;
+            }
+            g.drawImage(image, 0, 0, this);
+            this.repaint();
+        }
+
+    }
+
+    public static ImageFrame show(final byte[] image) throws IOException {
+        final ImageFrame frame = new ImageFrame(image);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        return frame;
+    }
+
+
+}
+
+
+
